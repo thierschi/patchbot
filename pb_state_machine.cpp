3c200be1599b5d315958c68e72d9cfc899751f10
Lukas Thiersch <lukas@barokas.de>
2020-02-20 15:50:22 +0100

    In case everything goes wrong...

--- pb_state_machine.cpp
+++ pb_state_machine.cpp
@@ -1,1 +1,136 @@
 #include "pb_state_machine.h"
+
+const terrain abstract_state_machine::free_tiles[] = {
+	terrain::ALIEN_GRASS,
+	terrain::ENEMY_START,
+	terrain::GRAVEL,
+	terrain::OPEN_AUTOMATIC_DOOR,
+	terrain::OPEN_MANUAL_DOOR,
+	terrain::PATCHBOT_START,
+	terrain::STEEL_PLANKS
+};
+
+const terrain abstract_state_machine::walls[] = {
+	terrain::AUTOMATIC_DOOR,
+	terrain::CONCRETE_WALL,
+	terrain::MANUAL_DOOR,
+	terrain::ROCK_WALL,
+	terrain::SECRET_PASSAGE
+};
+
+abstract_state_machine::abstract_state_machine(tile_map* map_, robot* self_) :
+	map(map_),
+	self(self_)
+{
+}
+
+bugger_ki::bugger_ki(tile_map* map_, robot* self_)
+{
+	map = map_;
+	self = self_;
+	machines_state = state::FW;
+	initialize_machine();
+}
+
+void bugger_ki::process()
+{
+}
+
+bugger_ki::event bugger_ki::get_event_at_FW() const
+{
+	coords auround_self[] = {
+		map->robots.get_robots_location(self->type),
+		map->robots.get_robots_location(self->type),
+		map->robots.get_robots_location(self->type),
+		map->robots.get_robots_location(self->type)
+	};
+
+	return (bugger_ki::event)0;
+}
+
+bugger_ki::event bugger_ki::get_event_at_W() const
+{
+	return (bugger_ki::event)0;
+}
+
+bugger_ki::event bugger_ki::get_event_at_FAW() const
+{
+	return (bugger_ki::event)0;
+}
+
+void bugger_ki::to_state_FW()
+{
+}
+
+void bugger_ki::to_state_W()
+{
+}
+
+void bugger_ki::to_state_FAW()
+{
+}
+
+void bugger_ki::initialize_machine()
+{
+	event_getter[state::FW] = &bugger_ki::get_event_at_FW;
+	event_getter[state::W] = &bugger_ki::get_event_at_W;
+	event_getter[state::FaW] = &bugger_ki::get_event_at_FAW;
+
+	transitions[state_event_pair(state::FW, event::zb_u_ns)] = &bugger_ki::to_state_W;
+	transitions[state_event_pair(state::FW, event::zf_u_ns)] = &bugger_ki::to_state_FW;
+	transitions[state_event_pair(state::FW, event::zs)] = &bugger_ki::to_state_FAW;
+
+	transitions[state_event_pair(state::W, event::zf)] = &bugger_ki::to_state_FW;
+	transitions[state_event_pair(state::W, event::zb)] = &bugger_ki::to_state_W;
+
+	transitions[state_event_pair(state::FaW, event::zw)] = &bugger_ki::to_state_FW;
+	transitions[state_event_pair(state::FaW, event::zf_u_nw)] = &bugger_ki::to_state_FAW;
+}
+
+pusher_ki::pusher_ki(tile_map* map_, robot* self_)
+{
+	map = map_;
+	self = self_;
+	machines_state = state::HB;
+	pusher_ki::initialize_machine();
+}
+
+void pusher_ki::process() 
+{
+}
+
+pusher_ki::event pusher_ki::get_event_at_HB() const
+{
+	return event::zf;
+}
+
+pusher_ki::event pusher_ki::get_event_at_VB() const
+{
+	return event::zf;
+}
+
+void pusher_ki::to_state_HB()
+{
+}
+
+void pusher_ki::to_state_VB()
+{
+}
+
+void pusher_ki::to_state_Z()
+{
+}
+
+void pusher_ki::initialize_machine()
+{
+	event_getter[state::HB] = &pusher_ki::get_event_at_HB;
+	event_getter[state::VB] = &pusher_ki::get_event_at_VB;
+
+	transitions[state_event_pair(state::HB, event::zf)] = &pusher_ki::to_state_HB;
+	transitions[state_event_pair(state::HB, event::zw_o_pbb)] = &pusher_ki::to_state_VB;
+	transitions[state_event_pair(state::HB, event::zg)] = &pusher_ki::to_state_Z;
+
+	transitions[state_event_pair(state::VB, event::zf)] = &pusher_ki::to_state_VB;
+	transitions[state_event_pair(state::VB, event::zw_o_pbh)] = &pusher_ki::to_state_HB;
+	transitions[state_event_pair(state::VB, event::zg)] = &pusher_ki::to_state_Z;
+}
--- pb_state_machine.h
+++ pb_state_machine.h
@@ -1,39 +1,90 @@
 #pragma once
+#include "pb_map.h"
 
-#include <unordered_map>
+#include <map>
+#include <functional>
 
-class state_machine
+class abstract_state_machine
 {
 public:
-	state_machine() = delete;
-	virtual ~state_machine() = 0;
+	abstract_state_machine() = default;
+	abstract_state_machine(tile_map* map_, robot* self_);
+	virtual ~abstract_state_machine() = default;
 
 	virtual void process() = 0;
 
 protected:
-	enum state;
-	enum event;
+	tile_map* map;
+	robot* self;
 
-	state machines_state;
-
-	typedef void (*state_func)();
-	typedef event (*check_func)();
-	typedef std::pair<state, event> state_event_pair;
-
-	std::unordered_map<state, check_func> event_getter;
-	std::unordered_map<state_event_pair, state_func> transitions;
+	static const terrain free_tiles[];
+	static const terrain walls[];
 
 	virtual void initialize_machine() = 0;
-
-	/*
-		Ich brauch noch eine Funktion, die das Event bekommt
-		Für jeden State eine Check funktion, die in Map gespeichert ist, diese gibt das event zurück
-
-		Maybeeee States als eigene Klasse -> NEIN
-	*/
 };
 
-class bugger_ki : public state_machine {
+class bugger_ki : public abstract_state_machine {
+public:
+	bugger_ki(tile_map* map_, robot* self_);
+	~bugger_ki() override = default;
 
+	void process() override;
+
+private:
+	const enum class state {FW, W, FaW };
+	const enum class event { zb_u_ns, zf, zb, zf_u_ns, zs, zw, zf_u_nw };
+
+	state machines_state;
+	coords target_tile;
+
+	typedef void (bugger_ki::* state_func)();
+	typedef bugger_ki::event(bugger_ki::* check_func)() const;
+	typedef std::pair<state, event> state_event_pair;
+	typedef std::pair<state, check_func> state_check_pair;
+
+	std::map<state, check_func> event_getter;
+	std::map<state_event_pair, state_func> transitions;
+
+	event get_event_at_FW() const;
+	event get_event_at_W() const;
+	event get_event_at_FAW() const;
+
+	void to_state_FW();
+	void to_state_W();
+	void to_state_FAW();
+
+	void initialize_machine() override;
+};
+
+class pusher_ki : public abstract_state_machine {
+public:
+	pusher_ki(tile_map* map_, robot* self_);
+	~pusher_ki() override = default;
+
+	void process();
+
+private:
+	const enum class state { HB, VB, Z };
+	const enum class event { zf, zw_o_pbb, zw_o_pbh, zg };
+
+	state machines_state;
+	coords target_tile;
+
+	typedef void (pusher_ki::* state_func)();
+	typedef pusher_ki::event (pusher_ki::* check_func)() const;
+	typedef std::pair<state, event> state_event_pair;
+	typedef std::pair<state, check_func> state_check_pair;
+
+	std::map<state, check_func> event_getter;
+	std::map<state_event_pair, state_func> transitions;
+
+	event get_event_at_HB() const;
+	event get_event_at_VB() const;
+
+	void to_state_HB();
+	void to_state_VB();
+	void to_state_Z();
+
+	void initialize_machine() override;
 };
 
